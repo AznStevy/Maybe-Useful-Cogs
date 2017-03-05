0ddb46b0494e05318aefd41bd9cd09bec61a822b
Stevy <AznStevy@users.noreply.github.com>
2017-03-05 12:50:10 -0500

    Embeds
    Got rid of things that broke it

--- markov/markov.py
+++ markov/markov.py
@@ -2,25 +2,20 @@ import discord
 from discord.ext import commands
 import random
 import os
-from .utils.dataIO import dataIO
+from .utils.dataIO import fileIO
 from cogs.utils import checks
-from __main__ import send_cmd_help
 
+prefix = fileIO("data/red/settings.json", "load")['PREFIXES'][0]
 
 class Markov:
     """A cog that generates text based on what your users say."""
 
     def __init__(self, bot):
         self.bot = bot
-        self.model = dataIO.load_json("data/markov/model.json")
-        self.settings = dataIO.load_json("data/markov/settings.json")
-
-    def save_json(self):
-        dataIO.save_json('data/markov/model.json', self.model)
-        dataIO.save_json('data/markov/settings.json', self.settings)
+        self.model = fileIO("data/markov/model.json", "load")
 
     @commands.command(pass_context=True, no_pm=True)
-    async def markov(self, ctx, *, msg=None):
+    async def markov(self,ctx, *, msg = None):
         """This isn't the text you want."""
 
         user = ctx.message.author
@@ -34,36 +29,32 @@ class Markov:
                 self.model[server.id][channel.id] = {}
 
             # generates sentence
-            if msg is None:
-                first_word = random.choice(list(self.model[server.id][channel.id].keys()))  # first word
+            if msg == None:
+                first_word = random.choice(list(self.model[server.id][channel.id].keys())) # first word
                 markov_text = first_word + " "
                 current_word = first_word
             else:
-                first_word = msg.split(" ")[-1]  # first word
+                first_word = msg.split(" ")[-1] # first word
                 markov_text = msg + " "
                 current_word = first_word
 
-            bad_chars = ['?', '\r', '.'] + self.bot.command_prefix
-            while not any(b in markov_text for b in bad_chars) and len(markov_text) < 200:
+            while '?' not in markov_text and '\r' not in markov_text and '.' not in markov_text and prefix not in markov_text and len(markov_text) < 200:
                 try:
                     new_word = random.choice(self.model[server.id][channel.id][current_word])
                     current_word = new_word
                     markov_text += new_word + " "
                 except:
                     break
+            em = discord.Embed(description='', colour=user.colour)
+            em.set_author(name="Generated Text", icon_url = user.avatar_url)
+            em.description = markov_text
 
-            await self.bot.say("**Generated Text: **{} ".format(markov_text))
+            await self.bot.say(embed = em)
 
-    @commands.group(pass_context=True, no_pm=True)
-    async def mconf(self, ctx):
-        """Configure markov cog"""
-        if ctx.invoked_subcommand is None:
-            await send_cmd_help(ctx)
-
-    @mconf.command(pass_context=True, no_pm=True, name='clear')
+    @commands.command(pass_context=True, no_pm=True)
     @checks.is_owner()
-    async def clear(self, ctx):
-        """Clears the model for specific channel."""
+    async def clear(self,ctx):
+        """Clears the data for specific channel."""
         user = ctx.message.author
         channel = ctx.message.channel
         server = user.server
@@ -74,35 +65,14 @@ class Markov:
             self.model[server.id][channel.id] = {}
 
         self.model[server.id][channel.id] = {}
-        self.save_json()
+        fileIO('data/markov/model.json', "save", self.model)          
         await self.bot.say("Channel:`{}` data cleared.".format(channel.name))
 
-    @mconf.command(no_pm=True)
-    @checks.is_owner()
-    async def toggle(self, ctx, channel: discord.Channel = None):
-        """Toggles markov generation for a certain channel (defaults to present)"""
-        server = ctx.message.server
-        channel = ctx.message.channel if channel else channel
-
-        if server.id not in self.settings:
-            self.settings[server.id] = {}
-        if channel.id not in self.settings[server.id]:
-            self.settings[server.id][channel.id] = False
-
-        newval = not self.settings[server.id][channel.id]
-        self.settings[server.id][channel.id] = newval
-
-        verbs = ('disabled', 'enabled')
-        await self.bot.say('Markov chain generation %s in %s' %
-                           (verbs[int(newval)], channel.mention))
-        self.save_json()
-
-    @mconf.command(no_pm=True)
-    @checks.is_owner()
+    @commands.command(no_pm=True)
     async def size(self):
         """Shows the size of the file."""
         size = os.path.getsize('data/markov/model.json')
-        await self.bot.say("Current File Size: `{}` Bytes.".format(size))
+        await self.bot.say("Current File Size: `{}` Bytes.".format(size))        
 
     # loads the new text into the model
     async def track_message(self, message):
@@ -112,15 +82,7 @@ class Markov:
             channel = message.channel
             user = message.author
 
-            if server.id not in self.settings:
-                self.settings[server.id] = {}
-            if channel.id not in self.settings[server.id]:
-                self.settings[server.id][channel.id] = False
-
-            if not self.settings[server.id][channel.id]:
-                return
-
-            if not user.bot and not any(text.startswith(p) for p in self.bot.command_prefix):
+            if not user.bot and not text.startswith(prefix):
                 words = text.split(" ")
 
                 if server.id not in self.model:
@@ -131,33 +93,28 @@ class Markov:
                 for i in range(len(words) - 1):
                     if words[i] not in self.model[server.id][channel.id]:
                         self.model[server.id][channel.id][words[i]] = list()
-                    self.model[server.id][channel.id][words[i]].append(words[i + 1])
+                    self.model[server.id][channel.id][words[i]].append(words[i+1])
 
-                self.save_json()
+                fileIO('data/markov/model.json', "save", self.model)
         except:
             pass
 
-# ------------------------------ setup ----------------------------------------
-
-
+# ------------------------------ setup ----------------------------------------    
 def check_folders():
     if not os.path.exists("data/markov"):
         print("Creating data/markov folder...")
         os.makedirs("data/markov")
 
-
 def check_files():
-    p = "data/markov/"
-    for f in ['model.json', 'settings.json']:
-        if not dataIO.is_valid_json(f):
-            print("Creating %s..." % f)
-            dataIO.save_json(p + f, {})
-
+    f = "data/markov/model.json"
+    if not fileIO(f, "check"):
+        print("Creating model.json...")
+        fileIO(f, "save", {})
 
 def setup(bot):
     check_folders()
     check_files()
 
     n = Markov(bot)
-    bot.add_listener(n.track_message, "on_message")
+    bot.add_listener(n.track_message,"on_message")
     bot.add_cog(n)
