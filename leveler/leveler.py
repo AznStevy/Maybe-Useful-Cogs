950a09320dde30cec1f10cf2df960e02d9a68b27
Stevy <AznStevy@users.noreply.github.com>
2017-03-27 02:44:01 -0400

    Add files via upload

--- leveler/leveler.py
+++ leveler/leveler.py
@@ -2174,7 +2174,7 @@ class Leveler:
         if "chat_block" not in userinfo:
             userinfo["chat_block"] = 0
 
-        if float(curr_time) - float(userinfo["chat_block"]) >= 10 and not any(text.startswith(x) for x in prefix):
+        if float(curr_time) - float(userinfo["chat_block"]) >= 120 and not any(text.startswith(x) for x in prefix):
             await self._process_exp(message, userinfo, random.randint(15, 20))
             await self._give_chat_credit(user, server)
         #except AttributeError as e:
