3178d255ffdfda6d918b9ae196ad7c0ce45f36cb
Stevy <AznStevy@users.noreply.github.com>
2017-04-01 14:40:58 -0400

    Bug fixes.

--- leveler/leveler.py
+++ leveler/leveler.py
@@ -41,7 +41,7 @@ default_avatar_url = "http://i.imgur.com/XPDO9VH.jpg"
 
 try:
     client = MongoClient()
-    db = client['leveler_dev']
+    db = client['leveler']
 except:
     print("Can't load database. Follow instructions on Git/online to install MongoDB.")
 
