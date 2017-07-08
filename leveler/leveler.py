00f45e96b2cc720a84649eaf6648f2be570ffc24
Stevy <AznStevy@users.noreply.github.com>
2017-07-08 13:34:07 -0400

    New levelup design.

--- leveler/leveler.py
+++ leveler/leveler.py
@@ -1350,9 +1350,9 @@ class Leveler:
                 else:
                     await self.bot.say('**{}, you already have this badge!**'.format(user.name))
             else:
-                await self.bot.say('**The badge `{}` does not exist. (try `{}lvlbadge available`)**'.format(name, prefix[0]))
+                await self.bot.say('**The badge `{}` does not exist. (try `{}badge available`)**'.format(name, prefix[0]))
         else:
-            await self.bot.say('**There are no badges to get! (try `{}lvlbadge get [name] -global`).**'.format(prefix[0]))
+            await self.bot.say('**There are no badges to get! (try `{}badge get [name] -global`).**'.format(prefix[0]))
 
     @badge.command(name="set", pass_context=True, no_pm=True)
     async def set(self, ctx, name:str, priority_num:int):
@@ -1906,7 +1906,7 @@ class Leveler:
                 await self.bot.say("**Invalid Background Type. (profile, rank, levelup)**")
 
     async def draw_profile(self, user, server):
-        name_fnt = ImageFont.truetype(font_bold_file, 22)
+        name_fnt = ImageFont.truetype(font_bold_file, 22, encoding="utf-8")
         header_u_fnt = ImageFont.truetype(font_unicode_file, 18, encoding="utf-8")
         title_fnt = ImageFont.truetype(font_file, 18, encoding="utf-8")
         sub_header_fnt = ImageFont.truetype(font_bold_file, 14, encoding="utf-8")
@@ -1926,7 +1926,7 @@ class Leveler:
 
             for char in text:
                 if char.isalnum() or char in string.punctuation or char in string.whitespace:
-                    draw.text((write_pos, y), char, font=font, fill=fill)
+                    draw.text((write_pos, y), u"{}".format(char), font=font, fill=fill)
                     write_pos += font.getsize(char)[0]
                 else:
                     draw.text((write_pos, y), u"{}".format(char), font=unicode_font, fill=fill)
@@ -2514,7 +2514,6 @@ class Leveler:
         alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
         im.putalpha(alpha)
         return im
-
     async def draw_levelup(self, user, server):
         userinfo = db.users.find_one({'user_id':user.id})
         # get urls
@@ -2542,31 +2541,63 @@ class Leveler:
         profile_image = Image.open('data/leveler/temp/{}_temp_level_profile.png'.format(user.id)).convert('RGBA')
 
         # set canvas
+        width = 175
+        height = 65
         bg_color = (255,255,255, 0)
-        result = Image.new('RGBA', (85, 105), bg_color)
-        process = Image.new('RGBA', (85, 105), bg_color)
+        result = Image.new('RGBA', (width, height), bg_color)
+        process = Image.new('RGBA', (width, height), bg_color)
 
         # draw
         draw = ImageDraw.Draw(process)
 
         # puts in background
-        bg_image = bg_image.resize((85, 105), Image.ANTIALIAS)
-        bg_image = bg_image.crop((0,0, 85, 105))
+        bg_image = bg_image.resize((width, height), Image.ANTIALIAS)
+        bg_image = bg_image.crop((0,0, width, height))
         result.paste(bg_image, (0,0))
 
         # draw transparent overlay
         if "levelup_info_color" in userinfo.keys():
             info_color = tuple(userinfo["levelup_info_color"])
-            info_color = (info_color[0], info_color[1], info_color[2], 160) # increase transparency
+            info_color = (info_color[0], info_color[1], info_color[2], 150) # increase transparency
         else:
-            info_color = (30, 30 ,30, 160)
-        draw.rectangle([(0, 40), (85, 105)], fill=info_color) # info portion
-        draw.rectangle([(15, 11), (68, 64)], fill=(255,255,255,160), outline=(100, 100, 100, 100)) # profile rectangle
+            info_color = (30, 30 ,30, 150)
+        draw.rectangle([(38, 5), (170, 60)], fill=info_color) # info portion
 
+        # draw level circle
+        multiplier = 6
+        lvl_circle_dia = 60
+        circle_left = 4
+        circle_top = int((height- lvl_circle_dia)/2)
+        raw_length = lvl_circle_dia * multiplier
+
+        # create mask
+        mask = Image.new('L', (raw_length, raw_length), 0)
+        draw_thumb = ImageDraw.Draw(mask)
+        draw_thumb.ellipse((0, 0) + (raw_length, raw_length), fill = 255, outline = 0)
+
+        # drawing level bar calculate angle
+        start_angle = -90 # from top instead of 3oclock
+
+        lvl_circle = Image.new("RGBA", (raw_length, raw_length))
+        draw_lvl_circle = ImageDraw.Draw(lvl_circle)
+        draw_lvl_circle.ellipse([0, 0, raw_length, raw_length], fill=(255, 255, 255, 220), outline = (255, 255, 255, 220))
+
+        # put on level bar circle
+        lvl_circle = lvl_circle.resize((lvl_circle_dia, lvl_circle_dia), Image.ANTIALIAS)
+        lvl_bar_mask = mask.resize((lvl_circle_dia, lvl_circle_dia), Image.ANTIALIAS)
+        process.paste(lvl_circle, (circle_left, circle_top), lvl_bar_mask)
+
+        # draws mask
+        total_gap = 6
+        border = int(total_gap/2)
+        profile_size = lvl_circle_dia - total_gap
+        raw_length = profile_size * multiplier
         # put in profile picture
-        profile_size = (50, 50)
-        profile_image = profile_image.resize(profile_size, Image.ANTIALIAS)
-        process.paste(profile_image, (17, 13))
+        output = ImageOps.fit(profile_image, (raw_length, raw_length), centering=(0.5, 0.5))
+        output = output.resize((profile_size, profile_size), Image.ANTIALIAS)
+        mask = mask.resize((profile_size, profile_size), Image.ANTIALIAS)
+        profile_image = profile_image.resize((profile_size, profile_size), Image.ANTIALIAS)
+        process.paste(profile_image, (circle_left + border, circle_top + border), mask)
 
         # fonts
         level_fnt2 = ImageFont.truetype('data/leveler/fonts/font_bold.ttf', 19)
@@ -2576,12 +2607,12 @@ class Leveler:
         white_text = (240,240,240,255)
         dark_text = (35, 35, 35, 230)
         level_up_text = self._contrast(info_color, white_text, dark_text)
-        draw.text((self._center(0, 85, "LEVEL UP!", level_fnt2), 67), "LEVEL UP!", font=level_fnt2, fill=level_up_text) # Level
-        lvl_text = "LVL {}".format(userinfo["servers"][server.id]["level"])
-        draw.text((self._center(2, 85, lvl_text, level_fnt), 83), lvl_text, font=level_fnt, fill=level_up_text) # Level Number
+        lvl_text = "LEVEL {}".format(userinfo["servers"][server.id]["level"])
+        draw.text((self._center(50, 170, lvl_text, level_fnt), 22), lvl_text, font=level_fnt, fill=level_up_text) # Level Number
 
         result = Image.alpha_composite(result, process)
-        result.save('data/leveler/temp/{}_level.png'.format(user.id),'PNG', quality=100)
+        filename = 'data/leveler/temp/{}_level.png'.format(user.id)
+        result.save(filename,'PNG', quality=100)
 
     async def _handle_on_message(self, message):
         #try:
